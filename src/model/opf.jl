09c736c81e93cb40f7e771098bd861bdf3cd2787
Carleton <cjc@lanl.gov>
2017-09-06 11:41:28 -0600

    adding soc-opf model

--- src/model/opf.jl
+++ src/model/opf.jl
@@ -1,4 +1,4 @@
-export post_ac_opf, post_dc_opf
+export post_ac_opf, post_soc_opf, post_dc_opf
 
 """
 Given a JuMP model and a PowerModels network data structure, 
@@ -45,7 +45,6 @@ function post_ac_opf(data::Dict{String,Any}, model=Model())
     end
 
     for (i,branch) in ref[:branch]
-        # AC Line Flow Constraint
         f_idx = (i, branch["f_bus"], branch["t_bus"])
         t_idx = (i, branch["t_bus"], branch["f_bus"])
 
@@ -65,12 +64,12 @@ function post_ac_opf(data::Dict{String,Any}, model=Model())
         c = branch["br_b"]
         tm = branch["tap"]^2
 
+        # AC Line Flow Constraints
         @NLconstraint(model, p_fr == g/tm*vm_fr^2 + (-g*tr+b*ti)/tm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-b*tr-g*ti)/tm*(vm_fr*vm_to*sin(va_fr-va_to)) )
         @NLconstraint(model, q_fr == -(b+c/2)/tm*vm_fr^2 - (-b*tr-g*ti)/tm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-g*tr+b*ti)/tm*(vm_fr*vm_to*sin(va_fr-va_to)) )
 
         @NLconstraint(model, p_to == g*vm_to^2 + (-g*tr-b*ti)/tm*(vm_to*vm_fr*cos(va_to-va_fr)) + (-b*tr+g*ti)/tm*(vm_to*vm_fr*sin(va_to-va_fr)) )
         @NLconstraint(model, q_to == -(b+c/2)*vm_to^2 - (-b*tr+g*ti)/tm*(vm_to*vm_fr*cos(va_fr-va_to)) + (-g*tr-b*ti)/tm*(vm_to*vm_fr*sin(va_to-va_fr)) )
-
 
         # Phase Angle Difference Limit
         @constraint(model, va_fr - va_to <= branch["angmax"])
@@ -93,6 +92,101 @@ function post_ac_opf(data::Dict{String,Any}, model=Model())
 end
 
 
+"""
+Given a JuMP model and a PowerModels network data structure, 
+Builds an SOC-OPF formulation of the given data and returns the JuMP model
+"""
+function post_soc_opf(data::Dict{String,Any}, model=Model())
+    ref = PMs.build_ref(data)
+
+    @variable(model, ref[:bus][i]["vmin"]^2 <= w[i in keys(ref[:bus])] <= ref[:bus][i]["vmax"]^2, start=1.001)
+
+    wr_min, wr_max, wi_min, wi_max = PMs.calc_voltage_product_bounds(ref[:buspairs])
+
+    @variable(model, wr_min[bp] <= wr[bp in keys(ref[:buspairs])] <= wr_max[bp], start=1.0)
+    @variable(model, wi_min[bp] <= wi[bp in keys(ref[:buspairs])] <= wi_max[bp])
+
+    @variable(model, ref[:gen][i]["pmin"] <= pg[i in keys(ref[:gen])] <= ref[:gen][i]["pmax"])
+    @variable(model, ref[:gen][i]["qmin"] <= qg[i in keys(ref[:gen])] <= ref[:gen][i]["qmax"])
+
+    @variable(model, -ref[:branch][l]["rate_a"] <= p[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+    @variable(model, -ref[:branch][l]["rate_a"] <= q[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+
+    @variable(model, ref[:arcs_dc_param][a]["pmin"] <= p_dc[a in ref[:arcs_dc]] <= ref[:arcs_dc_param][a]["pmax"])
+    @variable(model, ref[:arcs_dc_param][a]["qmin"] <= q_dc[a in ref[:arcs_dc]] <= ref[:arcs_dc_param][a]["qmax"])
+
+    from_idx = Dict(arc[1] => arc for arc in ref[:arcs_from_dc])
+    @objective(model, Min, 
+        sum(gen["cost"][1]*pg[i]^2 + gen["cost"][2]*pg[i] + gen["cost"][3] for (i,gen) in ref[:gen]) +
+        sum(dcline["cost"][1]*p_dc[from_idx[i]]^2 + dcline["cost"][2]*p_dc[from_idx[i]] + dcline["cost"][3] for (i,dcline) in ref[:dcline])
+    )
+
+    for (i,j) in keys(ref[:buspairs])
+        # Voltage Product Relaxation
+        @constraint(model, wr[(i,j)]^2 + wi[(i,j)]^2 <= w[i]*w[j])
+    end
+
+    for (i,bus) in ref[:bus]
+        # Bus KCL
+        @constraint(model, 
+            sum(p[a] for a in ref[:bus_arcs][i]) + 
+            sum(p_dc[a_dc] for a_dc in ref[:bus_arcs_dc][i]) == 
+            sum(pg[g] for g in ref[:bus_gens][i]) - bus["pd"] - bus["gs"]*w[i]
+        )
+        @constraint(model, 
+            sum(q[a] for a in ref[:bus_arcs][i]) + 
+            sum(q_dc[a_dc] for a_dc in ref[:bus_arcs_dc][i]) == 
+            sum(qg[g] for g in ref[:bus_gens][i]) - bus["qd"] + bus["bs"]*w[i]
+        )
+    end
+
+    for (i,branch) in ref[:branch]
+        f_idx = (i, branch["f_bus"], branch["t_bus"])
+        t_idx = (i, branch["t_bus"], branch["f_bus"])
+        bp_idx = (branch["f_bus"], branch["t_bus"])
+
+        p_fr = p[f_idx]
+        q_fr = q[f_idx]
+        p_to = p[t_idx]
+        q_to = q[t_idx]
+
+        w_fr = w[branch["f_bus"]]
+        w_to = w[branch["t_bus"]]
+        wr_br = wr[bp_idx]
+        wi_br = wi[bp_idx]
+
+        # Line Flow
+        g, b = PMs.calc_branch_y(branch)
+        tr, ti = PMs.calc_branch_t(branch)
+        c = branch["br_b"]
+        tm = branch["tap"]^2
+
+        # AC Line Flow Constraints
+        @constraint(model, p_fr == g/tm*w_fr + (-g*tr+b*ti)/tm*(wr_br) + (-b*tr-g*ti)/tm*(wi_br) )
+        @constraint(model, q_fr == -(b+c/2)/tm*w_fr - (-b*tr-g*ti)/tm*(wr_br) + (-g*tr+b*ti)/tm*(wi_br) )
+
+        @constraint(model, p_to == g*w_to + (-g*tr-b*ti)/tm*(wr_br) + (-b*tr+g*ti)/tm*(-wi_br) )
+        @constraint(model, q_to == -(b+c/2)*w_to - (-b*tr+g*ti)/tm*(wr_br) + (-g*tr-b*ti)/tm*(-wi_br) )
+
+        # Phase Angle Difference Limit
+        @constraint(model, wi_br <= tan(branch["angmax"])*wr_br)
+        @constraint(model, wi_br >= tan(branch["angmin"])*wr_br)
+
+        # Apparent Power Limit, From and To
+        @constraint(model, p[f_idx]^2 + q[f_idx]^2 <= branch["rate_a"]^2)
+        @constraint(model, p[t_idx]^2 + q[t_idx]^2 <= branch["rate_a"]^2)
+    end
+
+    for (i,dcline) in ref[:dcline]
+        # DC Line Flow Constraint
+        f_idx = (i, dcline["f_bus"], dcline["t_bus"])
+        t_idx = (i, dcline["t_bus"], dcline["f_bus"])
+
+        @constraint(model, (1-dcline["loss1"])*p_dc[f_idx] + (p_dc[t_idx] - dcline["loss0"]) == 0)
+    end
+
+    return model
+end
 
 """
 Given a JuMP model and a PowerModels network data structure, 
@@ -133,16 +227,15 @@ function post_dc_opf(data::Dict{String,Any}, model=Model())
     end
 
     for (i,branch) in ref[:branch]
-        # AC Line Flow Constraint
         f_idx = (i, branch["f_bus"], branch["t_bus"])
 
         p_fr = p[f_idx]
         va_fr = va[branch["f_bus"]]
         va_to = va[branch["t_bus"]]
 
-        # Line Flow
         g, b = PMs.calc_branch_y(branch)
 
+        # DC Line Flow Constraints
         @constraint(model, p_fr == -b*(va_fr - va_to))
 
         # Phase Angle Difference Limit
