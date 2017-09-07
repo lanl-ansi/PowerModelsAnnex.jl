19d0b568c6ec6949711a47a1f5fe054034c3e9f6
Carleton Coffrin <carleton@coffr.in>
2017-09-06 20:41:39 -0600

    SOC OPF and PF Models (#9)
    * added soc-opf model
    * added soc-pf formulation
    * added some tests
    * closes #8

--- test/model/opf.jl
+++ test/model/opf.jl
@@ -34,6 +34,44 @@ end
     end
 end
 
+function run_soc_opf_model(data, solver)
+    model = post_soc_opf(data, Model(solver=solver))
+    status = solve(model)
+    return status, model
+end
+
+@testset "test soc w opf" begin
+    @testset "case $(case_file)" for case_file in case_files
+        data = PMs.parse_file(case_file)
+        opf_status, opf_model = run_soc_opf_model(data, ipopt_solver)
+        pm_result = run_opf(data, SOCWRPowerModel, ipopt_solver)
+        pm_sol = pm_result["solution"]
+
+        base_mva = data["baseMVA"]
+
+        for (i, bus) in data["bus"]
+            if bus["bus_type"] != 4
+                index = parse(Int, i)
+                #println("$i, $(getvalue(opf_model[:va][index])), $(pm_sol["bus"][i]["va"])")
+                #@test isapprox(getvalue(opf_model[:va][index]), pm_sol["bus"][i]["va"]; atol = 1e-8)
+
+                #println("$i, $(getvalue(opf_model[:w][index])), $(pm_sol["bus"][i]["vm"]^2)")
+                @test isapprox(getvalue(opf_model[:w][index]), pm_sol["bus"][i]["vm"]^2; atol = 1e-6)
+            end
+        end
+
+        for (i, gen) in data["gen"]
+            if gen["gen_status"] != 0
+                index = parse(Int, i)
+                #println("$i, $(getvalue(opf_model[:pg][index])), $(pm_sol["gen"][i]["pg"])")
+                @test isapprox(getvalue(opf_model[:pg][index]), pm_sol["gen"][i]["pg"]; atol = 1e-6)
+                # multiple generators at one bus can cause this to be non-unqiue
+                #@test isapprox(getvalue(opf_model[:qg][index]), pm_sol["gen"][i]["qg"])
+            end
+        end
+    end
+end
+
 function run_dc_opf_model(data, solver)
     model = post_dc_opf(data, Model(solver=solver))
     status = solve(model)
--- test/model/pf.jl
+++ test/model/pf.jl
@@ -35,6 +35,46 @@ end
     end
 end
 
+function run_soc_pf_model(data, solver)
+    model = post_soc_pf(data, Model(solver=solver))
+    status = solve(model)
+    return status, model
+end
+
+@testset "test soc w pf" begin
+    @testset "case $(case_file)" for case_file in case_files
+        data = PMs.parse_file(case_file)
+        pf_status, pf_model = run_soc_pf_model(data, ipopt_solver)
+        pm_result = run_pf(data, SOCWRPowerModel, ipopt_solver)
+        pm_sol = pm_result["solution"]
+
+        #println(pf_status)
+        #println(pm_result["status"])
+
+        base_mva = data["baseMVA"]
+
+        for (i, bus) in data["bus"]
+            if bus["bus_type"] != 4
+                index = parse(Int, i)
+                #println("$i, $(getvalue(pf_model[:va][index])), $(pm_sol["bus"][i]["va"])")
+                #@test isapprox(getvalue(pf_model[:va][index]), pm_sol["bus"][i]["va"]; atol = 1e-8)
+                #println("$i, $(getvalue(pf_model[:w][index])), $(pm_sol["bus"][i]["vm"]^2)")
+                @test isapprox(getvalue(pf_model[:w][index]), pm_sol["bus"][i]["vm"]^2; atol = 1e-6)
+            end
+        end
+
+        for (i, gen) in data["gen"]
+            if gen["gen_status"] != 0
+                index = parse(Int, i)
+                #println("$i, $(getvalue(pf_model[:pg][index])), $(pm_sol["gen"][i]["pg"])")
+                @test isapprox(getvalue(pf_model[:pg][index]), pm_sol["gen"][i]["pg"]; atol = 1e-8)
+                # multiple generators at one bus can cause this to be non-unqiue
+                #@test isapprox(getvalue(pf_model[:qg][index]), pm_sol["gen"][i]["qg"])
+            end
+        end
+    end
+end
+
 function run_dc_pf_model(data, solver)
     model = post_dc_pf(data, Model(solver=solver))
     status = solve(model)
