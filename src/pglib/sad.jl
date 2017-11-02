bc04781713967a3f9ea603422af5b7b88a209691
carleton <cjc@lanl.gov>
2017-11-02 11:23:27 -0600

    updating pglib models to PowerModels v0.5

--- src/pglib/sad.jl
+++ src/pglib/sad.jl
@@ -9,7 +9,7 @@ end
 function post_sad_opf{T}(pm::GenericPowerModel{T})
     PMs.variable_voltage(pm)
     PMs.variable_generation(pm)
-    PMs.variable_line_flow(pm)
+    PMs.variable_branch_flow(pm)
     PMs.variable_dcline_flow(pm, bounded = false)
 
     @variable(pm.model, theta_delta_bound >= 0.0, start = 0.523598776)
@@ -18,30 +18,29 @@ function post_sad_opf{T}(pm::GenericPowerModel{T})
 
     PMs.constraint_voltage(pm)
 
-    for (i,bus) in pm.ref[:ref_buses]
-        PMs.constraint_theta_ref(pm, bus)
+    for i in ids(pm, :ref_buses)
+        PMs.constraint_theta_ref(pm, i)
     end
 
-    for (i,bus) in pm.ref[:bus]
-        PMs.constraint_kcl_shunt(pm, bus)
+    for i in ids(pm, :bus)
+        PMs.constraint_kcl_shunt(pm, i)
     end
 
-    for (i,branch) in pm.ref[:branch]
-        PMs.constraint_ohms_yt_from(pm, branch)
-        PMs.constraint_ohms_yt_to(pm, branch)
+    for (i, branch) in ref(pm, :branch)
+        PMs.constraint_ohms_yt_from(pm, i)
+        PMs.constraint_ohms_yt_to(pm, i)
 
-        PMs.constraint_voltage_angle_difference(pm, branch)
-        theta_fr = pm.var[:va][branch["f_bus"]]
-        theta_to = pm.var[:va][branch["t_bus"]]
-
+        PMs.constraint_voltage_angle_difference(pm, i)
+        theta_fr = var(pm, :va, branch["f_bus"])
+        theta_to = var(pm, :va, branch["t_bus"])
         @constraint(pm.model, theta_fr - theta_to <=  theta_delta_bound)
         @constraint(pm.model, theta_fr - theta_to >= -theta_delta_bound)
 
-        constraint_thermal_limit_from(pm, branch; scale = 0.999)
-        constraint_thermal_limit_to(pm, branch; scale = 0.999)
+        constraint_thermal_limit_from(pm, i; scale = 0.999)
+        constraint_thermal_limit_to(pm, i; scale = 0.999)
     end
 
-    for (i,dcline) in pm.ref[:dcline]
-        PMs.constraint_dcline(pm, dcline)
+    for i in ids(pm, :dcline)
+        PMs.constraint_dcline(pm, i)
     end
 end
--- src/pglib/shared.jl
+++ src/pglib/shared.jl
@@ -1,3 +1,4 @@
+#=
 ""
 function constraint_thermal_limit_from(pm::GenericPowerModel, branch; scale = 1.0)
     i = branch["index"]
@@ -17,3 +18,31 @@ function constraint_thermal_limit_to(pm::GenericPowerModel, branch; scale = 1.0)
 
     PMs.constraint_thermal_limit_to(pm, t_idx, branch["rate_a"]*scale)
 end
+=#
+
+
+### Branch - Thermal Limit Constraints ###
+
+""
+function constraint_thermal_limit_from(pm::GenericPowerModel, n::Int, i::Int; scale = 1.0)
+    branch = ref(pm, n, :branch, i)
+    f_bus = branch["f_bus"]
+    t_bus = branch["t_bus"]
+    f_idx = (i, f_bus, t_bus)
+
+    PMs.constraint_thermal_limit_from(pm, n, f_idx, branch["rate_a"]*scale)
+end
+constraint_thermal_limit_from(pm::GenericPowerModel, i::Int; scale = 1.0) = constraint_thermal_limit_from(pm, pm.cnw, i; scale=scale)
+
+""
+function constraint_thermal_limit_to(pm::GenericPowerModel, n::Int, i::Int; scale = 1.0)
+    branch = ref(pm, n, :branch, i)
+    f_bus = branch["f_bus"]
+    t_bus = branch["t_bus"]
+    t_idx = (i, t_bus, f_bus)
+
+    PMs.constraint_thermal_limit_to(pm, n, t_idx, branch["rate_a"]*scale)
+end
+constraint_thermal_limit_to(pm::GenericPowerModel, i::Int; scale = 1.0) = constraint_thermal_limit_to(pm, pm.cnw, i; scale=scale)
+
+
