bc6c6f765e645418c15772b0b756fab566cdcd37
ccoffrin <carleton@coffrin.com>
2017-11-09 21:40:31 -0700

    removing dead code [ci skip]

--- src/pglib/shared.jl
+++ src/pglib/shared.jl
@@ -1,26 +1,3 @@
-#=
-""
-function constraint_thermal_limit_from(pm::GenericPowerModel, branch; scale = 1.0)
-    i = branch["index"]
-    f_bus = branch["f_bus"]
-    t_bus = branch["t_bus"]
-    f_idx = (i, f_bus, t_bus)
-
-    PMs.constraint_thermal_limit_from(pm, f_idx, branch["rate_a"]*scale)
-end
-
-""
-function constraint_thermal_limit_to(pm::GenericPowerModel, branch; scale = 1.0)
-    i = branch["index"]
-    f_bus = branch["f_bus"]
-    t_bus = branch["t_bus"]
-    t_idx = (i, t_bus, f_bus)
-
-    PMs.constraint_thermal_limit_to(pm, t_idx, branch["rate_a"]*scale)
-end
-=#
-
-
 ### Branch - Thermal Limit Constraints ###
 
 ""
