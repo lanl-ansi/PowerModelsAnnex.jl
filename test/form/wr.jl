b423722381608d934a2753a1732b2ce6cf4e9529
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2018-08-09 12:57:26 -0700

    qc-lambda model added (#23)

--- test/form/wr.jl
+++ test/form/wr.jl
@@ -53,3 +53,32 @@
         @test isapprox(result["objective"], 172.41; atol = 1e0)
     end
 end
+
+
+
+@testset "test qc tri without linking" begin
+    @testset "3-bus case" begin
+        pm = build_generic_model(case_files["case3"], QCWRTriNoLinkPowerModel, PowerModels.post_opf)
+
+        result = solve_generic_model(pm, ipopt_solver)
+
+        @test result["status"] == :LocalOptimal
+        @test isapprox(result["objective"], 5817.58; atol = 1e0)
+    end
+    @testset "5-bus pjm case" begin
+        pm = build_generic_model(case_files["case5"], QCWRTriNoLinkPowerModel, PowerModels.post_opf)
+
+        result = solve_generic_model(pm, ipopt_solver)
+
+        @test result["status"] == :LocalOptimal
+        @test isapprox(result["objective"], 15051.6; atol = 1e2) 
+    end
+    @testset "30-bus ieee case" begin
+        pm = build_generic_model(case_files["case30"], QCWRTriNoLinkPowerModel, PowerModels.post_opf)
+
+        result = solve_generic_model(pm, ipopt_solver)
+
+        @test result["status"] == :LocalOptimal
+        @test isapprox(result["objective"], 173.806; atol = 1e0)
+    end
+end
